88631c272c38f0eef3ffdec72ea3bcc1c5336721
yuangan <411246903@qq.com>
2025-07-30 21:29:56 +0800

    add test celeba-hq

---
+++ Silencer-I/test_hallo_celebahq.sh
@@ -0,0 +1,127 @@
+# python test_hallo_celebahq.py 0 &
+# python test_hallo_celebahq.py 1 &
+# python test_hallo_celebahq.py 2 &
+# python test_hallo_celebahq.py 3 &
+
+# python test_hallo_affwild2.py 4 &
+# python test_hallo_affwild2.py 5 &
+# python test_hallo_affwild2.py 6 &
+# python test_hallo_affwild2.py 7 &
+
+# python test_hallo_celebahq_baseline.py 0 0 &
+# python test_hallo_celebahq_baseline.py 1 0 &
+# python test_hallo_celebahq_baseline.py 2 0 &
+# python test_hallo_celebahq_baseline.py 3 0 &
+
+# python test_hallo_celebahq_baseline.py 0 1 &
+# python test_hallo_celebahq_baseline.py 1 1 &
+# python test_hallo_celebahq_baseline.py 2 1 &
+# python test_hallo_celebahq_baseline.py 3 1 &
+
+# python test_hallo_celebahq_baseline.py 0 2 &
+# python test_hallo_celebahq_baseline.py 1 2 &
+# python test_hallo_celebahq_baseline.py 2 2 &
+# python test_hallo_celebahq_baseline.py 3 2 &
+
+# wait
+
+# python test_hallo_celebahq_baseline.py 0 3 &
+# python test_hallo_celebahq_baseline.py 1 3 &
+# python test_hallo_celebahq_baseline.py 2 3 &
+# python test_hallo_celebahq_baseline.py 3 3 &
+
+# python test_hallo_celebahq_baseline.py 0 4 &
+# python test_hallo_celebahq_baseline.py 1 4 &
+# python test_hallo_celebahq_baseline.py 2 4 &
+# python test_hallo_celebahq_baseline.py 3 4 &
+
+# wait
+
+# python test_hallo_celebahq_baseline.py 0 5 &
+# python test_hallo_celebahq_baseline.py 1 5 &
+# python test_hallo_celebahq_baseline.py 2 5 &
+# python test_hallo_celebahq_baseline.py 3 5 &
+
+# python test_hallo_celebahq_baseline.py 0 6 &
+# python test_hallo_celebahq_baseline.py 1 6 &
+# python test_hallo_celebahq_baseline.py 2 6 &
+# python test_hallo_celebahq_baseline.py 3 6 &
+
+python test_hallo_celebahq_hallo-.py 0 0 0 &
+python test_hallo_celebahq_hallo-.py 1 1 0 &
+python test_hallo_celebahq_hallo-.py 2 2 0 &
+python test_hallo_celebahq_hallo-.py 3 3 0 &
+
+# python test_hallo_celebahq_hallo-.py 0 1 1 &
+# python test_hallo_celebahq_hallo-.py 1 0 1 &
+# python test_hallo_celebahq_hallo-.py 2 3 1 &
+# python test_hallo_celebahq_hallo-.py 3 2 1 &
+
+# python test_hallo_celebahq_hallo-.py 0 0 4 &
+# python test_hallo_celebahq_hallo-.py 1 1 4 &
+# python test_hallo_celebahq_hallo-.py 2 2 4 &
+# python test_hallo_celebahq_hallo-.py 3 3 4 &
+
+# wait
+
+# python test_hallo_celebahq_hallo-.py 0 3 6 &
+# python test_hallo_celebahq_hallo-.py 1 2 6 &
+# python test_hallo_celebahq_hallo-.py 2 1 6 &
+# python test_hallo_celebahq_hallo-.py 3 0 6 &
+
+
+# python test_hallo_celebahq_hallo-.py 0 2 0 
+# python test_hallo_celebahq_hallo-.py 1 2 0 
+# python test_hallo_celebahq_hallo-.py 2 2 0 &
+# python test_hallo_celebahq_hallo-.py 3 0 0 &
+
+# python test_hallo_celebahq_diff_hallo-.py 0 0 0 &
+# python test_hallo_celebahq_diff_hallo-.py 1 3 0 &
+# python test_hallo_celebahq_diff_hallo-.py 2 2 0 &
+# python test_hallo_celebahq_diff_hallo-.py 3 1 0 &
+
+# python test_hallo_celebahq_diff_hallo-_rebuttal_audio2.py 0 0 0 &
+# python test_hallo_celebahq_diff_hallo-_rebuttal_audio2.py 1 0 0 &
+# python test_hallo_celebahq_diff_hallo-_rebuttal_audio2.py 2 1 0 &
+# python test_hallo_celebahq_diff_hallo-_rebuttal_audio2.py 3 1 0 &
+
+# python test_hallo_celebahq_purified.py 0 &
+# python test_hallo_celebahq_purified.py 1 &
+# python test_hallo_celebahq_purified.py 2 &
+# python test_hallo_celebahq_purified.py 3 &
+
+#### 11.13/14 run
+# python test_hallo_celebahq_purified2.py 0 &
+# python test_hallo_celebahq_purified2.py 1 &
+# python test_hallo_celebahq_purified2.py 2 &
+# python test_hallo_celebahq_purified2.py 3 &
+
+# python test_hallo_celebahq_diff_hallo-.py 0 3 0 &
+# python test_hallo_celebahq_diff_hallo-.py 1 2 0 &
+# python test_hallo_celebahq_diff_hallo-.py 2 1 0 &
+# python test_hallo_celebahq_diff_hallo-.py 3 0 0 &
+
+# python test_hallo_celebahq_ablation_timesteps.py 0 &
+# python test_hallo_celebahq_ablation_timesteps.py 1 &
+# python test_hallo_celebahq_ablation_timesteps.py 2 &
+# python test_hallo_celebahq_ablation_timesteps.py 3 &
+
+# python test_hallo_celebahq_rebuttal_audio2.py 0 &
+# python test_hallo_celebahq_rebuttal_audio2.py 1 &
+# python test_hallo_celebahq_rebuttal_audio2.py 2 &
+# python test_hallo_celebahq_rebuttal_audio2.py 3 &
+
+# python test_hallo_celebahq_diff_hallo-_time_step_18.py 0 3 0 &
+# python test_hallo_celebahq_diff_hallo-_time_step_18.py 1 2 0 &
+# python test_hallo_celebahq_diff_hallo-_time_step_18.py 2 1 0 &
+# python test_hallo_celebahq_diff_hallo-_time_step_18.py 3 0 0 &
+
+# python test_hallo_celebahq_diff_hallo-_rebuttal_audio2.py 0 0 0 &
+# python test_hallo_celebahq_diff_hallo-_rebuttal_audio2.py 1 0 0 &
+# python test_hallo_celebahq_diff_hallo-_rebuttal_audio2.py 2 1 0 &
+# python test_hallo_celebahq_diff_hallo-_rebuttal_audio2.py 3 1 0 &
+
+# python test_hallo_celebahq_rebuttal_audio2.py 0 &
+# python test_hallo_celebahq_rebuttal_audio2.py 1 &
+# python test_hallo_celebahq_rebuttal_audio2.py 2 &
+# python test_hallo_celebahq_rebuttal_audio2.py 3 &
--- Silencer-I/test_hallo_celebahq_hallo-.py
+++ Silencer-I/test_hallo_celebahq_hallo-.py
@@ -3,31 +3,32 @@ import sys
 from glob import glob
 from tqdm import tqdm
 
-prefix = int(sys.argv[2])
+prefix = int(sys.argv[3])
 
-allfolders = glob(f'./protect/out_th1kh_512/*_200')
-allimgs = glob(allfolders[prefix]+'/th1kh_imgs_100/*')
+
+allfolders = glob(f'./protect/out_celebahq_512/hallo_eps16_steps100_gmode-_200*') ### epsilon 16
+print(allfolders)
+allfolders.sort()
+allimgs = glob(allfolders[prefix]+'/celebahq_512/*')
 baselinename = os.path.basename(allfolders[prefix])
-outpath = f'./talking_output/th1kh_base_{baselinename}/'
+outpath = f'./talking_output/celebahq_{baselinename}/'
 os.makedirs(outpath, exist_ok=True)
-print('outpath: ', outpath)
 
 part = int(sys.argv[1])
-device = part
+device = int(sys.argv[2])
 
 ll = len(allimgs) //4 + 1
-print(part, ll)
+print(part, ll, baselinename)
 
 
 for f in tqdm(allimgs[ll*part: ll*(part+1)], desc=f'test part: {part}'):
     source_image = f
-    name = f.split('/')[-1][:-4]
-    driving_wav = f'./th1kh/th1kh_100/audios/{name}.wav'
-    out = os.path.join(outpath, 'hallo_'+name+'.mp4')
+    name = os.path.basename(f)[:-4]
+    driving_wav = './examples/driving_audios/1.wav'
+    out = os.path.join(outpath, name+'.mp4')
 
     if os.path.exists(out):
         print(f'exists {out}')
-        breakpoint()
+        continue
+    
     os.system(f'CUDA_VISIBLE_DEVICES={device} python scripts/inference.py --source_image {source_image} --driving_audio {driving_wav} --output {out}')
-
-
